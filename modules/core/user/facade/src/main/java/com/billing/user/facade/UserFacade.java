3ae37010963c605b673fa5e7dc9344a53cc64209
Xenox <xenoxpcp@gmail.com>
2014-08-26 18:30:26 +0800

    2014/08/21 Commit Test

--- modules/core/user/facade/src/main/java/com/billing/user/facade/UserFacade.java
+++ modules/core/user/facade/src/main/java/com/billing/user/facade/UserFacade.java
@@ -6,21 +6,68 @@ import com.billing.internalcontract.user.ActionTokenReq;
 import com.billing.internalcontract.user.IUserFacade;
 import com.billing.internalcontract.user.LoginReq;
 import com.billing.internalcontract.user.RegisterReq;
+import com.billing.user.orm.dao.CustomerLoginDao;
+import com.billing.user.orm.dao.CustomerTerminalDao;
+import com.billing.user.orm.model.CustomerLogin;
+
+import java.util.function.Predicate;
 
 /**
  * Created by zkai on 2014/8/26.
  */
 public class UserFacade implements IUserFacade {
+    private CustomerLoginDao loginDao;
+    private CustomerTerminalDao terminalDao;
     @Override
     public BaseResp register(RegisterReq registerReq) {
+
         return null;
     }
 
     @Override
     public BaseResp login(LoginReq loginReq) {
+        BaseResp baseResp = new BaseResp(true);
+        CustomerLogin customerLoginDao = null;
+        if(loginReq.isAuto()){
+
+        }else {
+            switch (loginReq.getLoginAccountType()) {
+                case Anonymous:
+                    break;
+                case LoginName:
+                     customerLoginDao = loginDao.getByLoginName(loginReq.getLoginAccount());
+                    // TODO:password salt
+                    if(loginReq.getPassword().equals(customerLoginDao.getCurrentPassword())){
+
+                    }else{
+                        baseResp.
+                    }
+                    break;
+                case LoginEmail:
+                     customerLoginDao = loginDao.getByLoginEmail(loginReq.getLoginAccount());
+                    // TODO:password salt
+                    if(loginReq.getPassword().equals(customerLoginDao.getCurrentPassword())){
+
+                    }else{
+
+                    }
+                    break;
+                case LoginPhone:
+                    customerLoginDao = loginDao.getByLoginPhone(loginReq.getLoginAccount());
+                    // TODO:password salt
+                    if(loginReq.getPassword().equals(customerLoginDao.getCurrentPassword())){
+
+                    }else{
+
+                    }
+                    break;
+            }
+        }
         return null;
     }
+    private static BaseResp chkPassword(Predicate condition) {
 
+    }
     @Override
     public void loginout(BaseReq baseReq) {
 
